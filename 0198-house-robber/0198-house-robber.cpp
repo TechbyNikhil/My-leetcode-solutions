1351e81786203c1e970c8f0b2ede35074c36ba20
Nikhil Pathak <nikhilpathak0099@gmail.com>
2026-08-13 15:48:55 +0530

    Time: 0 ms (100%), Space: 10 MB (90.28%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+
+        int notrob=0;
+        int rob=0;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            int newrob;
+            int newnotrob;
+            newnotrob=max(notrob,rob);
+            newrob=notrob+nums[i];
+
+            rob=newrob;
+            notrob=newnotrob;
+        }
+
+        return max(rob,notrob);
+        
+    }
+};
